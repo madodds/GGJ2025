8e486ff11d42f690baf06efdafac98fc0c4539b6
Matthew Dodds <madodds@gmail.com>
2025-01-26 14:32:20 -0700

    Cleanup of old code for getting the build running

--- Assets/Scripts/GameLogicManager.cs
+++ Assets/Scripts/GameLogicManager.cs
@@ -15,8 +15,6 @@ public enum CustomerStateEnum
 }
 public class GameLogicManager : MonoBehaviour
 {
-    // Public
-    public RandomSpriteManager spriteManager;
     public GameObject originalCustomer;
     public List<GameObject> customers;
     public AudioSource backgroundAudioSource;
